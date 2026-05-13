4a6b1348a16d3fe9fd771e487cc921f6476b273d
Pranay kailuke <rekhakailuke@gmail.com>
2026-05-13 08:58:35 +0530

    Backend setup complete

---
+++ backend/server.js
@@ -0,0 +1,29 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
+const dotenv = require("dotenv");
+
+dotenv.config();
+
+const app = express();
+
+// Middleware
+app.use(cors());
+app.use(express.json());
+
+// Test Route
+app.get("/", (req, res) => {
+    res.send("E-Shelf API is running!");
+});
+
+// Database Connection
+mongoose.connect(process.env.MONGO_URI)
+.then(() => {
+    console.log("MongoDB Connected!");
+    app.listen(process.env.PORT || 5000, () => {
+        console.log(`Server running on port 5000`);
+    });
+})
+.catch((error) => {
+    console.log("Error:", error);
+});
