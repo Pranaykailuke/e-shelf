a33d2216a2085151702b939bc6f227f6865e1404
Pranay kailuke <rekhakailuke@gmail.com>
2026-05-13 09:59:34 +0530

    Authentication APIs complete

--- backend/server.js
+++ backend/server.js
@@ -11,6 +11,10 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+// Routes
+const authRoutes = require("./routes/authRoutes");
+app.use("/api/auth", authRoutes);
+
 // Test Route
 app.get("/", (req, res) => {
     res.send("E-Shelf API is running!");
